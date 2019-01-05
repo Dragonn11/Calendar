663dc7fb2b15f70466e7decfbbe7019ec0702d4a
arnab <arit765@gmail.com>
2019-01-05 14:47:34 +0530

    Updates

---
+++ precache-manifest.1815ce4277b0f911597f00a381df63ca.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Calendar/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "1bc49e1111dc3e8434da",
+    "url": "/Calendar/static/css/main.f704d5ba.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Calendar/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a7ba82df7a723c7c81eb",
+    "url": "/Calendar/static/js/1.a7ba82df.chunk.js"
+  },
+  {
+    "revision": "efbf30f94ac7825eb22d",
+    "url": "/Calendar/static/js/runtime~main.efbf30f9.js"
+  },
+  {
+    "revision": "1bc49e1111dc3e8434da",
+    "url": "/Calendar/static/js/main.1bc49e11.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Calendar/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Calendar/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Calendar/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "a7ba82df7a723c7c81eb",
+    "url": "/Calendar/static/css/1.70d995d0.chunk.css"
+  },
+  {
+    "revision": "d25717d38a23892c57decd0f9ebfb8fe",
+    "url": "/Calendar/index.html"
+  }
+];
